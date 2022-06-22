cca3e3df333724e1c9378d78d2a1382048cb381e
stcouch <stuart.t.couch@gmail.com>
2022-06-22 20:28:01 +0000

    updated script

--- script.js
+++ script.js
@@ -7,49 +7,82 @@ let total = 0;
 //get buttons and modifiers
 const numberButtons = document.querySelectorAll('.number');
 const modButtons = document.querySelectorAll('.modifier');
-var equation = document.querySelector('.calc-screen-q');
-var totalfield = document.querySelector('.calc-screen-ans');
-var equals = document.querySelector('.equate');
+const equation = document.querySelector('.calc-screen-q');
+const totalfield = document.querySelector('.calc-screen-ans');
+const equals = document.querySelector('.equate');
+const clearButton = document.querySelector('.clear');
 
-for (let i = 0; i < numberButtons.length; i++){
-    numberButtons[i].addEventListener("click", function(){
-        totalfield.innerHTML += numberButtons[i].id;
-        value = totalfield.innerHTML;
-    });
-}
+//for (let i = 0; i < numberButtons.length; i++){
+//    numberButtons[i].addEventListener("click", function(){
+//        totalfield.innerHTML += numberButtons[i].id;
+//        value = totalfield.innerHTML;
+//    });
+//}
 
 //functions for add, subtract, multiply, divide, negate, clear and delete
 //or add to equate as mode?
+
+equals.addEventListener("click", () => evaluate(mode, value, total));
+
+clearButton.addEventListener("click", () => clear());
+
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => addDigit(button.textContent))
+)
 
 for (let i = 0; i < modButtons.length; i++){
     modButtons[i].addEventListener("click", function(){
         let mod = modButtons[i].id;
         if(mod == "multiply"){
-            equation.innerHTML = value + " * ";
-            totalfield.innerHTML = "";
+            equation.textContent = value + " * ";
+            total = value;
+            totalfield.textContent = "";
             mode = "multiply";
         }
         if(mod == "divide"){
-            equation.innerHTML = value + " / ";
-            totalfield.innerHTML = "";
+            equation.textContent = value + " / ";
+            total = value;
+            totalfield.textContent = "";
             mode = "divide";
         }
         if(mod == "subtract"){
-            equation.innerHTML = value + " - ";
-            totalfield.innerHTML = "";
+            equation.textContent = value + " - ";
+            total = value;
+            totalfield.textContent = "";
             mode = "subtract";
         }
         if(mod == "add"){
-            equation.innerHTML = value + " + ";
-            totalfield.innerHTML = "";
+            equation.textContent = value + " + ";
+            total = value;
+            totalfield.textContent = "";
             mode = "add";
         }
         console.log(mod);
     });
 }
 
-equals.addEventListener("click", function(){
+
+function addDigit(num){
+    totalfield.textContent += num;
+    value = totalfield.textContent;
+}
+
+function evaluate(mode, x, y){
+    console.log(x, y);
     switch(mode){
         case "multiply":
+            return x * y;
+        case "divide":
+            return x / y;
+        case "subtract":
+            return x - y;
+        case "add":
+            return x + y;
     }
-});
+}
+
+function clear(){
+    equation.textContent = "";
+    totalfield.textContent = "";
+    total = value = 0;
+}
